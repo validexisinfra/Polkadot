f8bfdbd53875134ae1dd6e1acf606921282973b7
VALIDEXIS <validexisglobal@gmail.com>
2025-05-11 22:31:10 +0300

    Create install-benchmark.sh

---
+++ install-benchmark.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+sudo apt update && sudo apt install -y \
+  build-essential clang cmake pkg-config libssl-dev git curl unzip \
+  protobuf-compiler libclang-dev llvm-dev
+
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+source $HOME/.cargo/env
+
+rustup update stable
+rustup target add wasm32-unknown-unknown
+rustup component add rust-src
+
+cargo install frame-omni-bencher
+
+git clone https://github.com/paritytech/polkadot-sdk.git
+cd polkadot-sdk
+
+cargo build --features runtime-benchmarks --release
+
+mkdir -p scripts
+curl https://raw.githubusercontent.com/paritytech/polkadot-sdk/refs/tags/polkadot-stable2412/substrate/.maintain/frame-weight-template.hbs \
+  --output scripts/frame-weight-template.hbs
+
+frame-omni-bencher v1 benchmark pallet \
+  --runtime target/release/wbuild/polkadot-runtime/polkadot_runtime.compact.compressed.wasm \
+  --pallet pallet_balances \
+  --extrinsic "*" \
+  --template scripts/frame-weight-template.hbs \
+  --output weights.rs
+
+echo "✅ Benchmark results saved in weights.rs"
+
+echo "🖥️ Running hardware benchmark..."
+cargo run --release --features=runtime-benchmarks --bin polkadot -- benchmark machine
