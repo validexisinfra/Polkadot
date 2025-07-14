1e4aa598adac4aa551d6112b895634d2be7589aa
VALIDEXIS <validexisglobal@gmail.com>
2025-07-14 14:50:35 +0300

    Create upgrademain.sh

---
+++ upgrademain.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+echo "Stopping Polkadot node service..."
+sudo systemctl stop polkadot.service
+
+echo "Updating Polkadot SDK repository..."
+cd $HOME/polkadot-sdk
+git fetch
+git checkout polkadot-v1.18.7
+
+echo "Rebuilding Polkadot..."
+cargo build --release
+
+echo "Restarting Polkadot node service..."
+sudo systemctl restart polkadot.service
+
+echo "📄 Showing live logs:"
+sudo journalctl -u polkadot.service -f
